9d3d2bdf59fde100fb5db3d1bb30a0e3dc747ee6
Andre Nascimento <andredossantosnascimento1510@gmail.com>
2023-10-11 15:17:19 -0300

    feat: Criar usuário e Logar

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,19 +1,35 @@
-import { Controller, Post, Res, Query } from '@nestjs/common';
+import { Controller, Post, Res, Query, Body } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
 import { UserDTO } from './user.dto';
 import { UserService } from './user.service';
 
-@Controller('user')
+@Controller()
 export class UserController {
   constructor(private service: UserService) {}
-  @Post()
+  @Post('/user')
   @ApiTags('Users')
-  public async create(@Query() data: UserDTO, @Res() response: Response) {
-    const { password, ...rest } = await this.service.create(data);
-    return response.status(200).json(rest);
+  public async create(@Body() data: UserDTO, @Res() response: Response) {
+    try {
+      const { password, ...rest } = await this.service.create(data);
+      return response.status(201).json(rest);
+    } catch (error) {
+      return response.status(500).json({
+        message: 'Erro ao cadastrar usuário',
+        error: error.message,
+      });
+    }
   }
+  @Post('/login')
+  @ApiTags('Users')
   public async login(@Query() data: UserDTO, @Res() response: Response) {
-    return response.status(200).json(data);
+    try {
+      return response.status(200).json(await this.service.login(data));
+    } catch (error) {
+      return response.status(500).json({
+        message: 'Erro ao cadastrar usuário',
+        error: error.message,
+      });
+    }
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,13 @@
+import { genSalt, hashSync, compareSync } from 'bcrypt';
 import { Inject, Injectable } from '@nestjs/common';
 import { UserDTO } from './user.dto';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
-import { genSalt, hashSync } from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
+import { config } from 'dotenv';
+
+config();
+
 @Injectable()
 export class UserService {
   constructor(
@@ -10,11 +15,45 @@ export class UserService {
   ) {}
   create(userDTO: UserDTO) {
     const salt = 10;
-    genSalt(salt, (error) => {
-      if (!error) {
-        userDTO.password = hashSync(userDTO.password, salt);
-      }
+    return this.repository.save({
+      username: userDTO.username,
+      password: hashSync(userDTO.password, salt),
     });
-    return this.repository.save(userDTO);
+  }
+
+  async login(userDTO: UserDTO) {
+    const user = await this.repository.findOneBy({
+      username: userDTO.username,
+    });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    if (compareSync(userDTO.password, user.password)) {
+      const access_token = jwt.sign(
+        {
+          username: user.username,
+          id: user.id,
+        },
+        process.env.SECRET_KEY,
+      );
+      const refresh_token = jwt.sign(
+        {
+          username: user.username,
+          id: user.id,
+        },
+        process.env.SECRET_KEY,
+        { expiresIn: '1h' },
+      );
+      return {
+        access_token,
+        refresh_token,
+        token_type: 'Bearer',
+        expires_in: 3600,
+      };
+    }
+
+    throw new Error('Senha inválida');
   }
 }
